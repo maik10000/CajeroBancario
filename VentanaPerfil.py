ad182811213d7d1c8adf02503d8641c48d1af6e6
maik10000 <91635577+maik10000@users.noreply.github.com>
2022-12-17 18:16:32 -0500

    interfaz perfil al 50% xd esta good

--- VentanaPerfil.py
+++ VentanaPerfil.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import tkinter.font as tkFont
 import tkinter.ttk as ttk
+from PIL import ImageTk,Image
 import util.Cajero as caje
 FONDO = "#fff"
 class ventanaPerfil(tk.Tk):
@@ -26,6 +27,27 @@ class ventanaPerfil(tk.Tk):
         labelPrice.place(x=761,y=56,width=367,height=50)
 
         #Botones de perfil
+        img = Image.open(r'iconos\coin-dollar.png')
+        img = img.resize((50, 50))
+        icono1 = ImageTk.PhotoImage(img)
+
+        img = Image.open(r'iconos\download2.png')
+        img = img.resize((50, 50))
+        icono2 = ImageTk.PhotoImage(img)
+
+        img = Image.open(r'iconos\cog.png')
+        img = img.resize((50, 50))
+        icono3 = ImageTk.PhotoImage(img)
+
+        img = Image.open(r'iconos\loop.png')
+        img = img.resize((50, 50))
+        icono4 = ImageTk.PhotoImage(img)
+
+        img = Image.open(r'iconos\file-text2.png')
+        img = img.resize((50, 50))
+        icono5 = ImageTk.PhotoImage(img)
+
+
         ttk.Style().theme_use('clam')
         ttk.Style().configure('pad.TButton', background="#017175", bordercolor='#eee')
         ttk.Style().configure('pad2.TButton', background="#A68633", bordercolor='#eee')
@@ -34,23 +56,31 @@ class ventanaPerfil(tk.Tk):
         ttk.Style().map('pad2.TButton', background=[('pressed', '#5E4D21'), ('active', '#8B7230')])
         ttk.Style().map('pad3.TButton', background=[('pressed', '#8D8C2F'), ('active', '#BBB939')])
 
-        botonRetiro = ttk.Button(self,style="pad.TButton")
-        botonDeposito = ttk.Button(self,style="pad.TButton")
-        botonAjustes = ttk.Button(self,style="pad2.TButton")
-        botonServicios = ttk.Button(self,style="pad.TButton")
-        botonMovimientos = ttk.Button(self,style="pad.TButton")
+
+        botonRetiro = ttk.Button(self,style="pad.TButton",image= icono1)
+        botonRetiro.image = icono1
+        botonDeposito = ttk.Button(self,style="pad.TButton",image=icono2)
+        botonDeposito.imagen = icono2
+        botonAjustes = ttk.Button(self,style="pad2.TButton",image= icono3)
+        botonAjustes.image  = icono3
+        botonTransferencia= ttk.Button(self,style="pad.TButton",image= icono4)
+        botonTransferencia.image = icono4
+        botonMovimientos = ttk.Button(self,style="pad.TButton",image= icono5)
+        botonMovimientos.image = icono5
+
         botonSalir = ttk.Button(self,style="pad3.TButton", text="SALIR")
 
         label = tk.Label(self,text= "Deposito",font= self.fontStyle3,bg=FONDO, foreground="#4D4D4D").place(x= 239, y= 240)
         label = tk.Label(self,text= "Retiro",font= self.fontStyle3,bg=FONDO, foreground="#4D4D4D").place(x= 239, y= 425)
         label = tk.Label(self,text= "Ajustes de Usuario",font= self.fontStyle3,bg=FONDO, foreground="#4D4D4D").place(x= 239, y= 597)
-        label = tk.Label(self,text= "Servicios Basicos",font= self.fontStyle3,bg=FONDO, foreground="#4D4D4D").place(x= 815, y= 240)
+        label = tk.Label(self,text= "Transferencias",font= self.fontStyle3,bg=FONDO, foreground="#4D4D4D").place(x= 815, y= 240)
         label = tk.Label(self,text= "Mis Movimientos",font= self.fontStyle3,bg=FONDO, foreground="#4D4D4D").place(x= 815, y= 425)
+
 
         botonRetiro.place(x=60, y=210, width=100, height=100)
         botonDeposito.place(x=60, y=400, width=100, height=100)
         botonAjustes.place(x=60, y=575, width=100, height=100)
-        botonServicios.place(x=635, y=210, width=100, height=100)
+        botonTransferencia.place(x=635, y=210, width=100, height=100)
         botonMovimientos.place(x=635, y=400, width=100, height=100)
         botonSalir.place(x=956, y=620, width=172, height=45)
 
@@ -65,7 +95,7 @@ class ventanaPerfil(tk.Tk):
     def ajustarUsuario(self):
         pass
 
-    def pagarServiciosb(self):
+    def transferir(self):
         pass
 
     def verMovimientos(self):
