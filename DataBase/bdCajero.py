d6eecdfb6fefeace0cac103af1c8807dac4f2e42
maik10000 <91635577+maik10000@users.noreply.github.com>
2022-12-21 13:43:48 -0500

    Implementacion
    Base de datos

--- DataBase/bdCajero.py
+++ DataBase/bdCajero.py
@@ -1,16 +1,14 @@
 import MySQLdb
 
-conexion = MySQLdb.connect(
-    host="localhost",
-    user="root",
-    passwd="",
-    database = "bdcajerobanco")
+def abrirConexion():
+    conexion = MySQLdb.connect(
+        host="localhost",
+        user="root",
+        passwd="",
+        database="bdcajerobanco")
+    return conexion
 
 
-puntero  = conexion.cursor()
-sql = """INSERT INTO provicia (ID_Prov,PROVINCIA) VALUES(%s,%s)"""
 
-conexion.commit()
-conexion.close()
 
 
